3618b53f5a41d06ab50d57a6777183a742b98337
raymondytian <rymdtian@gmail.com>
2024-04-09 09:32:46 -0500

    0.2.1

--- Formula/splix.rb
+++ Formula/splix.rb
@@ -1,9 +1,9 @@
 class Splix < Formula
     desc "Lightning-fast image splitter."
     homepage "https://github.com/raymondytian/splix"
-    url "https://github.com/raymondytian/splix/releases/download/v0.2.0/splix-v0.2.0.tar.gz"
-    sha256 "b48f4982cb6c30e3fd56a8109ce1492ed18a66d5e45b441e6bab7efcd4e67438"
-    version "0.2.0"
+    url "https://github.com/raymondytian/splix/releases/download/v0.2.1/splix-v0.2.1.tar.gz"
+    sha256 "ed9b6283fb6181271136816e422f838e2bf72dded926bb698b4bc6aa2d09c396"
+    version "0.2.1"
 
     def install
         bin.install "splix"
